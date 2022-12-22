e7a8d4e48b63032de371613cce8cee04b8ba79d8
moorts <moritz.gutfleisch@gmx.de>
2022-12-22 17:24:19 +0100

    Add LSP

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,4 +11,23 @@ require 'paq' {
     {'nvim-telescope/telescope.nvim', tag='0.1.0'};
     -- Highlighting
     {'nvim-treesitter/nvim-treesitter', {run=':TSUpdate'}};
+    -- Autocompletion
+    {'VonHeikemen/lsp-zero.nvim'};
+
+    -- LSP Support
+    {'neovim/nvim-lspconfig'};
+    {'williamboman/mason.nvim'};
+    {'williamboman/mason-lspconfig.nvim'};
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'};
+    {'hrsh7th/cmp-buffer'};
+    {'hrsh7th/cmp-path'};
+    {'saadparwaiz1/cmp_luasnip'};
+    {'hrsh7th/cmp-nvim-lsp'};
+    {'hrsh7th/cmp-nvim-lua'};
+
+    -- Snippets
+    {'L3MON4D3/LuaSnip'};
+    {'rafamadriz/friendly-snippets'};
 }
